3cb50f0fb2dce703760673e1adbc158a201b1332
fredericlfree <frederic.lefranc@asg.com>
2016-10-28 15:22:20 +0200

    module

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,20 @@ import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { AppModule } from './app/';
 
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Ng2aSidebarComponent } from './app/components/ng2a-sidebar/ng2a-sidebar.component';
+
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
+
+@NgModule({ 
+   declarations: [Ng2aSidebarComponent], 
+   imports: [CommonModule], 
+   exports: [Ng2aSidebarComponent] 
+}) 
+
+export class Ng2aSidebarModule {}
